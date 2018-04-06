9a90952a2561d28fef1ee0727be27cfbe15ab6af
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-04-05 23:45:02 -0800

    Added routes for user data

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,7 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/index');
 
-/* GET users listing. 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});*/
-
-// GET users 
+//E1: GET all users
 router.get('/', function (req, res, next) {
   model.users.findAll({})
       .then(users => res.json({
@@ -21,30 +16,85 @@ router.get('/', function (req, res, next) {
       }));
 }); 
 
+//E2: GET user by id
+router.get('/:id', function (req, res, next) {
+    model.users.findAll({
+        id: req.params.id
+    })
+        .then(users => res.json({
+            error: false,
+            data: users
+        }))
+        .catch(error => res.json({
+            error: true,
+            data: [],
+            error: error
+        }));
+  }); 
+
+//E3: Delete user by id
+router.delete('/:id', function (req, res, next) {
+    model.users.destroy({where: {
+        id: req.params.id
+    }})
+        .then(status => res.status(201).json({
+            error: false,
+            message: 'User has been deleted.'
+
+        }))
+        .catch(error => res.json({
+            error: true,
+            error: error
+        }))
+    });
 
 
-/* GET users listing
-router.get('/', function(req, res, next) {
- 
-});*/
- 
- 
-// POST user
-router.post('/', function(req, res, next) {
- 
-});
- 
- 
-// update user
-router.put('/:id', function(req, res, next) {
- 
-});
- 
- 
-// DELETE user
-router.delete('/:id', function(req, res, next) {
- 
+//E4: Add user
+//still need to validate email address
+router.post('/insert', function (req, res, next) {
+
+    model.users.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            created_at: new Date(),
+            updated_at: new Date()
+        })
+        .then(user => res.status(201).json({
+            error: false,
+            data: user,
+            message: 'New user created.'
+        }))
+        .catch(error => res.json({
+            error: true,
+            data: [],
+            error: error
+        }));
 });
 
+//E5: Update user
+// still need to add email validation
+router.post('/:id', function (req, res, next) {
+ 
+    model.users.update({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            updated_at: new Date()
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(user => res.status(201).json({
+            error: false,
+            message: 'User data updated.'
+        }))
+        .catch(error => res.json({
+            error: true,
+            error: error
+        }));
+});
+ 
 
 module.exports = router;
