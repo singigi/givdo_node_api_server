2fd654a7f2285161cf7af10e5490dfc48487c29b
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-05-06 13:25:25 -0800

    Corrected variable names in games routes to correspond with database for usability

--- routes/games.js
+++ routes/games.js
@@ -4,32 +4,38 @@ var model = require('../models/index');
 var check = require('express-validator/check');
 var games = model.games;
 
-//E1: GET all games by user_id
+//E1: GET all games by specified user_id
 
-router.get('/:user_id', function (req, res, next) {
+router.get('/:creator_user_id', function (req, res, next) {
 
     /**
      * Validations
      */
 
     // user_id validation
-    req.checkParams('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('User Id should be at least ' +
+    req.checkParams('creator_user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('creator_user_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
-    games.findOne({
-        attributes: ['id', 'created_at', 'updated_at', 'finished_at', 'single_player'],
-        where:{
-            creator_user_id: req.params.user_id
-        }})
+    var errors = req.validationErrors();
+    if(errors){
+        res.json(errors);
+    }
+    else {
+        games.findAll({
+            attributes: ['id', 'created_at', 'updated_at', 'finished_at', 'single_player'],
+            where:{
+                creator_user_id: req.params.creator_user_id
+            }})
         .then(games => res.json({
-        error: false,
-        data: games
-    }))
-.catch(error => res.json({
-        error: true,
-        data: [],
-        error: error
-    }));
+            error: false,
+            data: games
+        }))
+        .catch(error => res.json({
+            error: true,
+            data: [],
+            error: error
+        }));
+    }
 });
 
 //E2: Add a game record
@@ -40,7 +46,7 @@ router.post('/insert',function (req, res, next) {
      */
 
     // user_id validation
-    req.checkBody('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('User Id should be at least ' +
+    req.checkBody('creator_user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('creator_user_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
     // single_player validation
@@ -53,7 +59,7 @@ router.post('/insert',function (req, res, next) {
     }
     else {
         games.create({
-            creator_user_id: req.body.user_id,
+            creator_user_id: req.body.creator_user_id,
             single_player: req.body.single_player,
             created_at: new Date(),
             updated_at: new Date(),
@@ -72,21 +78,23 @@ router.post('/insert',function (req, res, next) {
     }
 });
 
+
+
 //E3: Update a game record by game_id
 
 /** Need to rethink about this route before deployment **/
-router.put('/:id', function (req, res, next) {
+router.put('/:game_id', function (req, res, next) {
 
     /**
      * Validations
      */
 
     // id validation
-    req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
+    req.checkParams('game_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('game_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
     // user_id validation
-    req.checkBody('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('User Id should be at least ' +
+    req.checkBody('creator_user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('creator_user_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
     // single_player validation
@@ -103,8 +111,8 @@ router.put('/:id', function (req, res, next) {
             updated_at: new Date()
         }, {
             where: {
-                id: req.params.id,
-                creator_user_id: req.body.user_id
+                game_id: req.params.game_id,
+                creator_user_id: req.body.creator_user_id
             }
         })
         .then(game => res.status(201).json({
@@ -113,7 +121,7 @@ router.put('/:id', function (req, res, next) {
         }))
         .catch(error => res.json({
             error: true,
-            error: error
+            message: error
         }));
     }
 });
