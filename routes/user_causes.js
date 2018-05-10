c50b1251935ccf45d8d692add8f549fa3eb89014
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-05-10 11:08:13 -0800

    Naming, formatting, typos

--- routes/user_causes.js
+++ routes/user_causes.js
@@ -5,18 +5,18 @@ var sequelize = model.sequelize;
 var check = require('express-validator/check');
 var user_causes = model.user_causes;
 
-//E1: GET all user_causes by user_id
+//?1: GET user_causes by user_id
 router.get('/:user_id', function (req, res, next) {
 
     /**
      * Validations
      */
 
-        // user_id validation
-    req.checkParams('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('User Id should be at least ' +
+    req.checkParams('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('user_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
-    var errors = req.validationErrors();
+    
+    ar errors = req.validationErrors();
     if(errors){
         res.json(errors);
     }
@@ -36,63 +36,21 @@ router.get('/:user_id', function (req, res, next) {
     }
 });
 
-//E2: Inactivate a user_cause by cause_id and user_id
-/***** MUST UPDATE DOCS *****/
-router.delete('/:cause_id/:user_id', function (req, res, next) {
 
-    /**
-     * Validations
-     */
-
-    // cause_id validation
-    req.checkParams('cause_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Cause Id should be at least ' +
-        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
-
-    // user_id validation
-    req.checkParams('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('User Id should be at least ' +
-        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
-
-    var errors = req.validationErrors();
-    if(errors){
-        res.json(errors);
-    }
-    else {
-        user_causes.update({
-                active: 0,
-                updated_at: new Date()
-            }, {
-                where: {
-                    cause_id: req.params.cause_id,
-                    user_id: req.params.user_id
-                }
-            })
-            .then(user_cause => res.status(201).json({
-            error: false,
-            message: 'User Cause inactivated (to permanently delete all record of this ad, use database tools).'
-        }))
-    .catch(error => res.json({
-            error: true,
-            message: error
-        }));
-    }
-});
-
-
-//E3: Add user_cause
+//?2: Add user_cause
 router.post('/insert',function (req, res, next) {
 
     /**
      * Validations
      */
 
-    // cause_id validation
-    req.checkParams('cause_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Cause Id should be at least ' +
+    req.checkParams('cause_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('cause_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
-    // user_id validation
-    req.checkParams('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('User Id should be at least ' +
+    req.checkParams('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('user_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
+    
     var errors = req.validationErrors();
     if(errors){
         res.json(errors);
@@ -113,6 +71,45 @@ router.post('/insert',function (req, res, next) {
             error: true,
             data: [],
             error: error
+        }));
+    }
+});
+
+//?3: Inactivate a user_cause by user_id and cause_id 
+router.delete('/:user_id/:cause_id', function (req, res, next) {
+
+    /**
+     * Validations
+     */
+
+    req.checkParams('cause_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('cause_id should be at least ' +
+        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
+
+    req.checkParams('user_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('user_id should be at least ' +
+        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
+
+    
+    var errors = req.validationErrors();
+    if(errors){
+        res.json(errors);
+    }
+    else {
+        user_causes.update({
+                active: 0,
+                updated_at: new Date()
+            }, {
+                where: {
+                    cause_id: req.params.cause_id,
+                    user_id: req.params.user_id
+                }
+            })
+            .then(user_cause => res.status(201).json({
+            error: false,
+            message: 'User Cause inactivated (to permanently delete all record, use database tools).'
+        }))
+    .catch(error => res.json({
+            error: true,
+            message: error
         }));
     }
 });
