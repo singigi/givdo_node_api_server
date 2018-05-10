dbba9bc2b15369877bfa7e1ff5217f279c6c32c9
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-05-10 12:06:10 -0800

    Minor formatting and validation revisions to badges routes

--- routes/badges.js
+++ routes/badges.js
@@ -4,10 +4,10 @@ var model = require('../models/index');
 var check = require('express-validator/check');
 var badges = model.badges;
 
-//E1: GET all badges
+//?1: GET all badges
 router.get('/', function (req, res, next) {
     badges.findAll({
-        attributes: ['id', 'name', 'image_link', 'score'],
+        attributes: ['id', 'name', 'image_link', 'score', 'created_at', 'updated_at'],
         where: {'active': 1}
     })
     .then(badges => res.json({
@@ -21,33 +21,14 @@ router.get('/', function (req, res, next) {
     }));
 });
 
-//E2: GET badge by id
-router.get('/:id', function (req, res, next) {
-    badges.findAll({
-        attributes: ['id', 'name', 'image_link', 'score'],
-        where:{
-            id: req.params.id,
-            'active': 1
-        }})
-        .then(badges => res.json({
-        error: false,
-        data: badges
-    }))
-    .catch(error => res.json({
-        error: true,
-        data: [],
-        error: error
-    }));
-});
 
-//E3: Delete badge by id
-router.delete('/:id', function (req, res, next) {
+//?2: GET badge by id
+router.get('/:id', function (req, res, next) {
 
     /**
      * Validations
      */
 
-    // id validation
     req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
@@ -56,45 +37,42 @@ router.delete('/:id', function (req, res, next) {
         res.json(errors);
     }
     else {
-        badges.update({
-            active: 0,
-            updated_at: new Date()
-        }, {
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(badge => res.status(201).json({
+        badges.findAll({
+            attributes: ['id', 'name', 'image_link', 'score', 'created_at', 'updated_at'],
+            where:{
+                id: req.params.id,
+                'active': 1
+            }})
+            .then(badges => res.json({
             error: false,
-            message: 'Badge inactivated (to permanently delete all record, use database tools).'
+            data: badges
         }))
         .catch(error => res.json({
             error: true,
-            message: error
+            data: [],
+            error: error
         }));
     }
 });
 
 
-//E4: Add badge
+//?3: Add badge
 router.post('/insert',function (req, res, next) {
 
     /**
      * Validations
      */
 
-    // name validation
     req.checkBody('name').trim().escape().isLength({ min: 3, max: 255 }).withMessage('Name should be at least ' +
         '3 chars and at most 255 chars').matches(/^[a-z\s]+$/i).withMessage('Only alphabets are allowed');
 
-    // image_link validation
     req.checkBody('image_link').trim().escape().isLength({ min: 15, max: 255 }).withMessage('Image Link should be at least ' +
         '15 chars and at most 255 chars');
 
-    // score validation
     req.checkBody('score').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Score should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
+    
     var errors = req.validationErrors();
     if(errors){
         res.json(errors);
@@ -120,29 +98,26 @@ router.post('/insert',function (req, res, next) {
     }
 });
 
-//E5: Update badge
+//?4: Update badge
 router.put('/:id', function (req, res, next) {
 
     /**
      * Validations
      */
 
-    // id validation
     req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
-    // name validation
     req.checkBody('name').trim().escape().isLength({ min: 3, max: 255 }).withMessage('Name should be at least ' +
         '3 chars and at most 255 chars').matches(/^[a-z\s]+$/i).withMessage('Only alphabets are allowed');
 
-    // image_link validation
     req.checkBody('image_link').trim().escape().isLength({ min: 15, max: 255 }).withMessage('Image Link should be at least ' +
         '15 chars and at most 255 chars');
 
-    // score validation
     req.checkBody('score').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Score should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
+    
     var errors = req.validationErrors();
     if(errors){
         res.json(errors);
@@ -168,6 +143,41 @@ router.put('/:id', function (req, res, next) {
         }
     }
 );
+
+//?5: Delete badge by id
+router.delete('/:id', function (req, res, next) {
+
+    /**
+     * Validations
+     */
+
+    req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
+        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
+
+    
+    var errors = req.validationErrors();
+    if(errors){
+        res.json(errors);
+    }
+    else {
+        badges.update({
+            active: 0,
+            updated_at: new Date()
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(badge => res.status(201).json({
+            error: false,
+            message: 'Badge inactivated (to permanently delete all record, use database tools).'
+        }))
+        .catch(error => res.json({
+            error: true,
+            message: error
+        }));
+    }
+});
 
 
 module.exports = router;
