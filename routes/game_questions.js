34c6fa4dc4f5de97ac69f02d8ad3fb8e9eb4e5f6
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-05-10 12:52:51 -0800

    Removed unnecessary game_questions route; validation and formatting changes

--- routes/game_questions.js
+++ routes/game_questions.js
@@ -4,32 +4,36 @@ var model = require('../models/index');
 var check = require('express-validator/check');
 var game_questions = model.game_questions;
 
-//E1: GET all game_questions by game_id
-
+//?1: GET game_question records by game_id
 router.get('/:game_id', function (req, res, next) {
 
     /**
      * Validations
      */
 
-    // game_id validation
-    req.checkParams('game_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Game Id should be at least ' +
+    req.checkParams('game_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('game_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
-    game_questions.findAll({
-        attributes: ['question_id', 'created_at', 'updated_at'],
-        where:{
-            game_id: req.params.game_id
-        }})
-        .then(game_questions => res.json({
-            error: false,
-            data: game_questions
-        }))
-        .catch(error => res.json({
-            error: true,
-            data: [],
-            error: error
-        }));
+    var errors = req.validationErrors();
+    if(errors){
+        res.json(errors);
+    } 
+    else {
+        game_questions.findAll({
+            attributes: ['game_id', 'question_id', 'created_at', 'updated_at'],
+            where:{
+                game_id: req.params.game_id
+            }})
+            .then(game_questions => res.json({
+                error: false,
+                data: game_questions
+            }))
+            .catch(error => res.json({
+                error: true,
+                data: [],
+                error: error
+            }));
+    }  
 });
 
 //E2: Add a game_question record
@@ -39,14 +43,13 @@ router.post('/insert',function (req, res, next) {
      * Validations
      */
 
-    // game_id validation
-    req.checkBody('game_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Game Id should be at least ' +
+    req.checkBody('game_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('game_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
-    // question_id validation
-    req.checkBody('question_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Question Id should be at least ' +
+    req.checkBody('question_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('question_id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
+    
     var errors = req.validationErrors();
     if(errors){
         res.json(errors);
@@ -61,53 +64,12 @@ router.post('/insert',function (req, res, next) {
         .then(game_question => res.status(201).json({
             error: false,
             data: question,
-            message: 'New Game Question created.'
+            message: 'New game_question record created.'
         }))
         .catch(error => res.json({
             error: true,
             data: [],
             error: error
-        }));
-    }
-});
-
-//E3: Update a game_question record by id and game_id
-
-/** Need to rethink about this route before deployment **/
-router.put('/:game_id', function (req, res, next) {
-
-    /**
-     * Validations
-     */
-
-    // game_id validation
-    req.checkParams('game_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Game Id should be at least ' +
-        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
-
-    // question_id validation
-    req.checkBody('question_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Question Id should be at least ' +
-        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
-
-    var errors = req.validationErrors();
-    if(errors){
-        res.json(errors);
-    }
-    else {
-        game_questions.update({
-            question_id: req.body.question_id,
-            updated_at: new Date()
-        }, {
-            where: {
-                game_id: req.body.game_id
-            }
-        })
-        .then(game_question => res.status(201).json({
-            error: false,
-            message: 'Game Question Record updated.'
-        }))
-        .catch(error => res.json({
-            error: true,
-            message: error
         }));
     }
 });
