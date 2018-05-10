2be3883c555e006cbd47b2837ea6e7d18b147ae9
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-05-10 14:13:10 -0800

    Validation and code ordering

--- routes/question_categories.js
+++ routes/question_categories.js
@@ -4,10 +4,10 @@ var model = require('../models/index');
 var check = require('express-validator/check');
 var question_categories = model.question_categories;
 
-//E1: GET all question_categories
+//?1: GET all question_category records
 router.get('/', function (req, res, next) {
     question_categories.findAll({
-            attributes: ['id', 'name'],
+            attributes: ['id', 'name', 'created_at', 'updated_at'],
             where: {'active': 1}
         })
         .then(question_categories => res.json({
@@ -21,10 +21,24 @@ router.get('/', function (req, res, next) {
     }));
 });
 
-//E2: GET question_category by id
+//?2: GET question_category record by id
 router.get('/:id', function (req, res, next) {
-    question_categories.findAll({
-            attributes: ['id', 'name'],
+
+    /**
+     * Validations
+     */
+
+    req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
+        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
+
+    
+        var errors = req.validationErrors();
+    if(errors){
+        res.json(errors);
+    }
+    else {
+        question_categories.findAll({
+            attributes: ['id', 'name', 'created_at', 'updated_at'],
             where:{
                 id: req.params.id,
                 'active': 1
@@ -38,17 +52,88 @@ router.get('/:id', function (req, res, next) {
         data: [],
         error: error
     }));
+}
+    
 });
 
-//E3: Inactivate question_category by id
-/***** MUST UPDATE DOCS *****/
+
+//?3: Add question_category record
+router.post('/insert',function (req, res, next) {
+
+    /**
+     * Validations
+     */
+
+    req.checkBody('name').trim().escape().isLength({ min: 3, max: 255 }).withMessage('Name should be at least ' +
+        '3 chars and at most 255 chars').matches(/^[a-z\s]+$/i).withMessage('Only alphabet characters are allowed');
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.json(errors);
+    }
+    else {
+        question_categories.create({
+                name: req.body.name,
+                created_at: new Date(),
+                updated_at: new Date()
+            })
+            .then(question_category => res.status(201).json({
+            error: false,
+            data: question_category,
+            message: 'New Question Category created.'
+        }))
+        .catch(error => res.json({
+            error: true,
+            data: [],
+            error: error
+        }));
+    }
+});
+
+//?4: Update question_category record
+router.put('/:id', function (req, res, next) {
+
+        /**
+         * Validations
+         */
+
+        req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
+            '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
+
+        req.checkBody('name').trim().escape().isLength({ min: 3, max: 255 }).withMessage('Name should be at least ' +
+            '3 chars and at most 255 chars').matches(/^[a-z\s]+$/i).withMessage('Only alphabet characters are allowed');
+
+        var errors = req.validationErrors();
+        if(errors){
+            res.json(errors);
+        }
+        else {
+            question_categories.update({
+                    name: req.body.name,
+                }, {
+                    where: {
+                        id: req.params.id
+                    }
+                })
+                .then(question_category => res.status(201).json({
+                error: false,
+                message: 'Question Category data updated.'
+            }))
+            .catch(error => res.json({
+                error: true,
+                error: error
+            }));
+        }
+    }
+);
+
+//?5: Inactivate question_category record by id
 router.delete('/:id', function (req, res, next) {
 
     /**
      * Validations
      */
 
-    // id validation
     req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
@@ -75,80 +160,6 @@ router.delete('/:id', function (req, res, next) {
         }));
     }
 });
-
-
-//E4: Add question_category
-router.post('/insert',function (req, res, next) {
-
-    /**
-     * Validations
-     */
-
-    // name validation
-    req.checkBody('name').trim().escape().isLength({ min: 3, max: 255 }).withMessage('Name should be at least ' +
-        '3 chars and at most 255 chars').matches(/^[a-z\s]+$/i).withMessage('Only alphabets are allowed');
-
-    var errors = req.validationErrors();
-    if(errors){
-        res.json(errors);
-    }
-    else {
-        question_categories.create({
-                name: req.body.name,
-                created_at: new Date(),
-                updated_at: new Date()
-            })
-            .then(question_category => res.status(201).json({
-            error: false,
-            data: question_category,
-            message: 'New Question Category created.'
-        }))
-        .catch(error => res.json({
-            error: true,
-            data: [],
-            error: error
-        }));
-    }
-});
-
-//E5: Update question_category
-router.put('/:id', function (req, res, next) {
-
-        /**
-         * Validations
-         */
-
-        // id validation
-        req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
-            '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
-
-        // name validation
-        req.checkBody('name').trim().escape().isLength({ min: 3, max: 255 }).withMessage('Name should be at least ' +
-            '3 chars and at most 255 chars').matches(/^[a-z\s]+$/i).withMessage('Only alphabets are allowed');
-
-        var errors = req.validationErrors();
-        if(errors){
-            res.json(errors);
-        }
-        else {
-            question_categories.update({
-                    name: req.body.name,
-                }, {
-                    where: {
-                        id: req.params.id
-                    }
-                })
-                .then(question_category => res.status(201).json({
-                error: false,
-                message: 'Question Category data updated.'
-            }))
-            .catch(error => res.json({
-                error: true,
-                error: error
-            }));
-        }
-    }
-);
 
 
 module.exports = router;
