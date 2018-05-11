db6dadc0d0b5f3a0a510e0e8091eff7d5ad8b6bd
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-05-11 11:12:32 -0800

    Slight changes to questions routes

--- routes/questions.js
+++ routes/questions.js
@@ -4,10 +4,10 @@ var model = require('../models/index');
 var check = require('express-validator/check');
 var questions = model.questions;
 
-//E1: GET all questions
+//?1: GET all questions
 router.get('/', function (req, res, next) {
     questions.findAll({
-            attributes: ['id', 'question_text', 'category_id'],
+            attributes: ['id', 'question_text', 'category_id', 'created_at', 'updated_at'],
             where: {'active': 1}
         })
         .then(questions => res.json({
@@ -21,10 +21,10 @@ router.get('/', function (req, res, next) {
     }));
 });
 
-//E2: GET question by id
+//?2: GET question by id
 router.get('/:id', function (req, res, next) {
     questions.findAll({
-            attributes: ['id', 'question_text', 'category_id'],
+            attributes: ['id', 'question_text', 'category_id', 'created_at', 'updated_at'],
             where:{
                 id: req.params.id,
                 'active': 1
@@ -40,43 +40,9 @@ router.get('/:id', function (req, res, next) {
     }));
 });
 
-//E3: Delete question by id
-router.delete('/:id', function (req, res, next) {
-
-    /**
-     * Validations
-     */
-
-    // id validation
-    req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
-        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
-
-    var errors = req.validationErrors();
-    if(errors){
-        res.json(errors);
-    }
-    else {
-        questions.update({
-            active: 0,
-            updated_at: new Date()
-        }, {
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(question => res.status(201).json({
-            error: false,
-            message: 'Question inactivated (to permanently delete all record, use database tools).'
-        }))
-        .catch(error => res.json({
-            error: true,
-            message: error
-        }));
-    }
-});
 
 
-//E4: Add question
+//?3: Add question
 router.post('/insert',function (req, res, next) {
 
     /**
@@ -90,6 +56,7 @@ router.post('/insert',function (req, res, next) {
     // category_id validation
     req.checkBody('category_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Category Id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
+
 
     var errors = req.validationErrors();
     if(errors){
@@ -115,24 +82,22 @@ router.post('/insert',function (req, res, next) {
     }
 });
 
-//E5: Update question
+//?4: Update question
 router.put('/:id', function (req, res, next) {
 
     /**
      * Validations
      */
 
-    // id validation
     req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
 
-    // question validation
     req.checkBody('question_text').trim().escape().isLength({ min: 10, max: 2000 }).withMessage('Question should be at least ' +
         '10 chars and at most 2000 chars');
 
-    // category_id validation
     req.checkBody('category_id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Category Id should be at least ' +
         '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
+
 
     var errors = req.validationErrors();
     if(errors){
@@ -151,6 +116,42 @@ router.put('/:id', function (req, res, next) {
          .then(question => res.status(201).json({
             error: false,
             message: 'Question data updated.'
+        }))
+        .catch(error => res.json({
+            error: true,
+            message: error
+        }));
+    }
+});
+
+//?5: Inactivate question by id
+router.delete('/:id', function (req, res, next) {
+
+    /**
+     * Validations
+     */
+
+    // id validation
+    req.checkParams('id').trim().escape().isLength({ min: 1, max: 11 }).withMessage('Id should be at least ' +
+        '1 chars and at most 11 chars').isInt().withMessage('Only numeric values are allowed');
+
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.json(errors);
+    }
+    else {
+        questions.update({
+            active: 0,
+            updated_at: new Date()
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(question => res.status(201).json({
+            error: false,
+            message: 'Question inactivated (to permanently delete all record, use database tools).'
         }))
         .catch(error => res.json({
             error: true,
