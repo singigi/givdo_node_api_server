32021fb2209318a869c94f4cca997199c723f9c2
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-05-26 15:38:31 -0800

    Fixed absent-minded code omission error

--- utils.js
+++ utils.js
@@ -1,5 +1,15 @@
-checkFacebookUser = function(accessToken, refreshToken, profile, cb) {
+/* 
+    This file contains some global methods to help with oauth 
+*/
+
+
+
+var methods = {};
+
+methods.checkFacebookUser = function(accessToken, refreshToken, profile, cb) {
     var that = this;
     console.log("checkFacebookUser called - need to implement this method in utils.js");
     //Check to see if the users exist in our database and add if not. Return to the callback function.
 };
+
+module.exports = methods;
