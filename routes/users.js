4acc4fe9f11346176623dba790d2fc237daf7891
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-06-23 16:52:20 -0800

    Removed old, unused methods

--- routes/users.js
+++ routes/users.js
@@ -173,27 +173,4 @@ router.delete('/:id', function (req, res, next) {
     }
 );
 
-
-/* 
-    Some utility methods to help with oauth:
- 
-
-var methods = {};
-
-methods.checkFacebookUser = function(accessToken, refreshToken, profile, cb) {
-    var that = this;
-    console.log("checkFacebookUser called - need to implement this method in utils.js");
-    //Check to see if the users exist in our database and add if not. Return to the callback function.
-};
-
-
-module.exports = {
-    checkFacebookUser: function(accessToken, refreshToken, profile, cb) {
-        var that = this;
-        console.log("checkFacebookUser called - need to implement this method in utils.js");
-    },
-};*/
-  
-
-//module.exports = methods;
 module.exports = router;
