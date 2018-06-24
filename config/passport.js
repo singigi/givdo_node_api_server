c14b452893cd1696fb106ce8a95c091515ca95f8
Tatiana McGarry <tatiana.mcgarry@gmail.com>
2018-06-23 17:24:03 -0800

    Added detailed explanatory comments

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,25 @@
+/*
+    This is where we configure the Passport strategies. We have two strategies: one for direct Facebook login, and one
+    which decodes and verifies JSON web tokens (JWTs) in order to log in a previously authorized user whose token is not yet expired.
+
+    With the first strategy, our client app obtains authorization from the user for our app (which is registered on Facebook) to access their 
+    Facebook profile, and Facebook sends an access token to the client app. The access token uniquely identifies the user. The client then sends 
+    this access token to us (the server), and we pass it in to the FacebookTokenStrategy below, along with our Facebook app id (which the user 
+    has authorized via the client app). 
+    
+    That strategy will send the access token to Facebook for them to verify that we are the intended recipient of the user's information, 
+    and they will return the user's Facebook data to us. We will then pass that data to our utils.checkFacebookUser method and see if the user
+    already exists in our database. If they do, we return their existing Givdo profile.
+
+    If the user does not exist yet, we create a record for them in our database and return the new profile, along with a JSON web token, which the client 
+    needs to store in the browser's local storage for authentication for protected routes.
+
+    The second strategy simply takes a JWT sent by the client, decodes it, and checks to see if it is valid and unexpired. Our valid JWTs have the user 
+    profile information encoded within them. If it is valid, we look up the user in our database and return thier Givdo profile data. If not, the
+    call will fail and the user will have to log in with the Facebook login route.
+
+*/
+
 'use strict';
 var passport = require('passport');
 var FacebookTokenStrategy = require('passport-facebook-token');
@@ -16,6 +38,7 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('JWT');
 opts.secretOrKey = jwt_config.secret;           //We need to change this for production (storing our key in the repo is not secure).
 
 
+//JWT login
 passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
     console.log('\x1b[36m%s\x1b[0m', 'Jwt decoded, looking for user in our database');  //Print in cyan
     console.log('\x1b[0m'), '';     //Reset console text color to default
